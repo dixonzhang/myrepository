8e324a607a6140aa889d0bee5e7d661660dc1f80
dixonzhang <758621681@qq.com>
2014-09-17 23:53:07 +0800

    delete some bean, remove to common

--- game/ddz-server/src/main/java/com/dixon/game/ddz/enu/ColourType.java
+++
@@ -1,6 +0,0 @@
-package com.dixon.game.ddz.enu;
-
-public enum ColourType {
-	heitao,hongxin,meihua,fangzhuan,
-	dawang,xiaowang;
-}
--- game/ddz-server/src/main/java/com/dixon/game/ddz/enu/RespType.java
+++
@@ -1,5 +0,0 @@
-package com.dixon.game.ddz.enu;
-
-public enum RespType {
-	error,notify,deskList,deskFull,deskInfo,playFirst,playFollow,win;
-}
